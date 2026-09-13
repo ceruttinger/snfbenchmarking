800b4c22063af41f82375f63f75589551383e413
ceruttinger <ceruttinger@gmail.com>
2026-09-13 00:15:08 -0600

    Make v0.41 config national-report aware

--- scripts/00_config.R
+++ scripts/00_config.R
@@ -8,11 +8,16 @@ ensure_packages(c("dplyr", "tidyr", "readr", "stringr", "tibble", "purrr", "json
 
 snf_v041_config <- function() {
   state_filter <- Sys.getenv("SNF_DASHBOARD_STATE", unset = "UT")
+  output_prefix <- Sys.getenv("SNF_OUTPUT_PREFIX", unset = "utah")
+  target_ccns_raw <- Sys.getenv("SNF_TARGET_CCNS", unset = "")
+  target_ccns <- if (nzchar(target_ccns_raw)) trimws(strsplit(target_ccns_raw, ",", fixed = TRUE)[[1]]) else character(0)
   peer_minimum <- suppressWarnings(as.integer(Sys.getenv("SNF_PEER_MINIMUM", unset = "5")))
   if (is.na(peer_minimum) || peer_minimum < 1) peer_minimum <- 5L
 
   list(
     state_filter = state_filter,
+    output_prefix = output_prefix,
+    target_ccns = target_ccns,
     peer_minimum = peer_minimum,
     project_version = "v0.41",
     processed_dir = "data/processed",
@@ -24,8 +29,8 @@ snf_v041_config <- function() {
     latest_valid_output = file.path("data/processed", "snf_latest_valid_reports_v041.rds"),
     metric_dictionary_config = file.path("data/config", "snf_metric_dictionary_v041.csv"),
     metric_dictionary_output = file.path("data/processed", "snf_metric_dictionary_v041.csv"),
-    peer_benchmark_output = file.path("data/processed", "snf_peer_benchmarks_v041.rds"),
-    peer_lookup_output = file.path("data/processed", "snf_peer_lookup_v041.rds")
+    peer_benchmark_output = file.path("data/processed", paste0("snf_peer_benchmarks_v041_", output_prefix, ".rds")),
+    peer_lookup_output = file.path("data/processed", paste0("snf_peer_lookup_v041_", output_prefix, ".rds"))
   )
 }
 
